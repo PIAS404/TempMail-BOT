fc6e0136f081ed4dd01611382803f3d89ac18abc
PIAS <193390371+PIAS404@users.noreply.github.com>
2026-05-19 00:39:04 +0600

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,7 @@
 from supabase import create_client, Client
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from backend.config import SUPABASE_URL, SUPABASE_KEY
 
 # Supabase Client
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 print("✅ Supabase Connected Successfully!")
